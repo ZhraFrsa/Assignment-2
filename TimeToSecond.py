feaa912755501bb7a7609340806650eeabd2b72a
ZhraFrsa <91796370+ZhraFrsa@users.noreply.github.com>
2021-10-09 11:33:50 -0400

    Add files via upload

---
+++ TimeToSecond.py
@@ -0,0 +1,13 @@
+print("***Convert Time to Second***")
+
+print("please enter Time: ")
+
+hour = int(input('enter hour:'))
+
+minute = int(input('enter minute:'))
+
+second = int(input('enter second:'))
+
+sec = hour*3600 + minute*60 + second
+
+print('seconds = ', sec)
